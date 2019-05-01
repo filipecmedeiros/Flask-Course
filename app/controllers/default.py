43508e4c64c3c94c1b3385243609ce1945806178
Filipe Medeiros <fcma@cin.ufpe.br>
2019-05-01 16:55:09 -0300

    Adding routes

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,15 @@
 from app import app
 
-
+@app.route('/index')
 @app.route('/')
 def index():
     return 'Hello World!'
+
+
+@app.route('/test', defaults={'user_id':None})
+@app.route('/test/<int:user_id>', methods=['GET'])
+def test(user_id):
+    if user_id:
+        return 'Olá, %s!' % user_id
+    else:
+        return 'Olá, usuário!'
