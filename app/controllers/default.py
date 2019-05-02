d691ab34d65aaa37b48c9cfbc942ef0f988696a7
Filipe Medeiros <fcma@cin.ufpe.br>
2019-05-02 20:29:41 -0300

    Creating template inheritance

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,16 +1,12 @@
 from flask import render_template
 from app import app
 
-@app.route('/index/<user>')
-@app.route('/', defaults={'user':None})
-def index(user):
-    return render_template('index.html', user=user)
+@app.route('/index')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-@app.route('/test', defaults={'user_id':None})
-@app.route('/test/<int:user_id>', methods=['GET'])
-def test(user_id):
-    if user_id:
-        return 'Olá, %s!' % user_id
-    else:
-        return 'Olá, usuário!'
+@app.route('/login')
+def login():
+    return render_template('base.html')
